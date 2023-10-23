7d8620cda38913b00781ecaf866070f46dd19854
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2023-10-23 05:58:45 +0200

    refactor(rotate-device-info): Optimised, cleaned the component

--- src/app/rotate-device-info/rotate-device-info.component.ts
+++ src/app/rotate-device-info/rotate-device-info.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+/**
+ * A component to display information and prompt the user to rotate their device.
+ * This  application that have a preferred orientation.
+ */
 @Component({
-  selector: 'app-rotate-device-info',
-  templateUrl: './rotate-device-info.component.html',
-  styleUrls: ['./rotate-device-info.component.scss']
+    selector: 'app-rotate-device-info',
+    templateUrl: './rotate-device-info.component.html',
+    styleUrls: ['./rotate-device-info.component.scss'],
 })
-export class RotateDeviceInfoComponent {
-
-}
+export class RotateDeviceInfoComponent {}
