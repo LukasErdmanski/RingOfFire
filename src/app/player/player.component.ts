4d67fc69cc8ae09c7e0546c56b7e43532bd87f85
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2023-10-23 06:05:14 +0200

    refactor(player): Optimised, cleaned the component

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,16 +1,38 @@
 import { Component, Input } from '@angular/core';
 
+/**
+ * Represents an individual player in the game.
+ *
+ * This component displays the player's name, status (whether they're active or not),
+ * their associated image, and also the state of the slide container.
+ */
 @Component({
-  selector: 'app-player',
-  templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss'],
+    selector: 'app-player',
+    templateUrl: './player.component.html',
+    styleUrls: ['./player.component.scss'],
 })
 export class PlayerComponent {
-  /* Für die Eingabe des Playernamen und ob der Player gerade aktiv (dran) (= true) ist aus dem 'game.component.html '*/
-  @Input() name!: string;
-  /* Am Anfang ist ein Spieler nicht aktiv. Diese Variable steuert auch die Aktivität/Vorhandensein 
-  der SCCS Klasse 'player-active' für aktiven Spieler in 'player.component.html' */
-  @Input() playerActive: boolean = false;
-  @Input() image: string = '1.webp';
-  @Input() playersSlideContainerHidden: boolean = false;
+    /**
+     * The name of the player.
+     * This is assigned in the parent component (e.g., 'game.component.html').
+     */
+    @Input() name!: string;
+
+    /**
+     * Indicates whether the player is currently active (taking their turn).
+     * Controls the presence/activity of the SCSS class 'player-active' in the template.
+     */
+    @Input() playerActive: boolean = false;
+
+    /**
+     * The image associated with the player.
+     * By default, the image is set to '1.webp'.
+     */
+    @Input() image: string = '1.webp';
+
+    /**
+     * Indicates the state of the players slide container.
+     * This is used to control visibility in the UI.
+     */
+    @Input() playersSlideContainerHidden: boolean = false;
 }
