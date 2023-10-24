4bcefa093fe96a9db275af1e88415987255d51f4
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2023-10-24 13:39:48 +0200

    M14: 2. Changed base href in index.html for diverse deployments, moved DA ftp deployment to angular-project folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
+/**
+ * Root application component.
+ *
+ * This component serves as the main entry point for the Angular application.
+ * It's typically used to hold application-wide components such as navigation bars, footers, or side menus.
+ * In this instance, it sets the title of the application.
+ */
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+    selector: 'app-root', // CSS selector that identifies this component in a template
+    templateUrl: 'app.component.html', // Path to the component's template
+    styleUrls: ['app.component.scss'], // Array of stylesheet URLs for this component
 })
 export class AppComponent {
-  title = 'LE - Ring of fire';
-  contentVisible = true;
-
-  constructor() {
-  }
+    /** The title of the application. */
+    title = 'LE - Ring of fire';
 }
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,37 +1,86 @@
-/* Analog wie bei ElPoloLoco hier werden Modelle angelegt. Unterschied: Properties werden validiert mittels Typisierung. */
-/* Man schreibt export vor class, damit diese woanders verwendet werden kann. */
-/* Für die einzelnen Properties der Klasse muss der Visibilty Parameter eingestellt werden. */
+/**
+ * Represents the state and mechanics of a card game.
+ * This class holds properties related to the game, players, cards, and their states.
+ *
+ * Properties are validated using typing.
+ * The 'export' keyword before 'class' makes it accessible elsewhere.
+ * Each property of the class should have its visibility modifier set.
+ */
 export class Game {
-    /* Initilisierung von Game Objekt Properties. */
+    /** Unique identifier for the game. */
     public id: string = '';
+
+    /** Array of player names participating in the game. */
     public players: string[] = [];
+
+    /** Array of images associated with each player. */
     public playerImages: string[] = [];
+
+    /** Stack of unplayed cards. */
     public stack: string[] = [];
+
+    /** Stack of cards that have already been played. */
     public playedCards: string[] = [];
+
+    /** Array indicating the mapping of cards from right to bottom stack. */
     public cardsRightToBottomStack: number[] = [0, 1, 2, 3];
+
+    /** Index of the player who currently has the turn. */
     public currentPlayer: number = 0;
-    /* Ergänzen um diese Props (Animationsvariable und Variable der aktuellen Karte) für Synchronisierung dieser Reaktionen auf anderen Geräten. */
-    public pickCardAnimation: boolean = false;
-    public currentCard: string = '';
+
+    /** Indicates if the game has concluded. */
     public gameOver: boolean = false;
+
+    /** ID for a new game. Used when the current game concludes. */
     public newGameId: string = '';
 
+    /**
+     * Properties like pickCardAnimation variables and the current card
+     * are included for synchronizing animations and reactions across devices.
+     */
+    /** Indicates if the pick card animation is currently active. */
+    public pickCardAnimation: boolean = false;
+    /** Represents the current card in play. */
+    public currentCard: string = '';
+
+    /** Predefined profile pictures available for players. */
     public allProfilesPictures: string[] = ['1.webp', '2.png', 'monkey.png', 'pinguin.svg', 'serious-woman.svg', 'winkboy.svg'];
 
+    /**
+     * Constructs a new instance of the `Game` class.
+     * Initializes the card stack and shuffles it.
+     */
     constructor() {
-        /* Fügt 52 Karten von 4 Kartensroten in 'stack' array hinzu. */
-        for (let i = 1; i < 2; i++) {
-            this.stack.push('spade_' + i);
-            this.stack.push('hearts_' + i);
-            this.stack.push('clubs_' + i);
-            // this.stack.push('diamonds_' + i);
-        }
-
+        this.initializeStack();
         this.shuffle(this.stack);
     }
 
-    /* Methode von: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array 
-    mischt zufällig alle 52 Spielkarten bei jedem Spiel erneut durch. */
+    /**
+     * Initializes the card stack with 52 cards from 4 card suits.
+     * @private
+     */
+    private initializeStack(): void {
+        // Comment the line below out and comment this line in to make tests and reach a fast game over after only three card draws. 
+        // for (let i = 1; i < 2; i++) {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('spade_' + i);
+            this.stack.push('hearts_' + i);
+            this.stack.push('clubs_' + i);
+            //  Comment the line below out to make tests and reach a fast game over after only three card draws. 
+            this.stack.push('diamonds_' + i); // Currently commented out.
+        }
+    }
+
+    /**
+     * Shuffles the provided array of strings in place.
+     *
+     * @param array - The array to shuffle.
+     * @returns The shuffled array.
+     *
+     * @remarks
+     * This method was adapted from a solution provided on StackOverflow.
+     * https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+     */
     shuffle(array: string[]) {
         let currentIndex = array.length,
             randomIndex;
@@ -49,7 +98,11 @@ export class Game {
         return array;
     }
 
-    /* Convert Game Object to Json. */
+    /**
+     * Converts the current state of the `Game` object into a plain JavaScript object.
+     *
+     * @returns The JSON representation of the game.
+     */
     public toJson() {
         return {
             id: this.id,
@@ -59,11 +112,14 @@ export class Game {
             playedCards: this.playedCards,
             cardsRightToBottomStack: this.cardsRightToBottomStack,
             currentPlayer: this.currentPlayer,
-            /* Ergänzen um diese Props (Animationsvariable und Variable der aktuellen Karte) für Synchronisierung dieser Reaktionen auf anderen Geräten. */
-            pickCardAnimation: this.pickCardAnimation,
-            currentCard: this.currentCard,
             gameOver: this.gameOver,
             newGameId: this.newGameId,
+            /**
+             * Properties like pickCardAnimation variables and the current card
+             * are included for synchronizing animations and reactions across devices.
+             */
+            pickCardAnimation: this.pickCardAnimation,
+            currentCard: this.currentCard,
         };
     }
 }
